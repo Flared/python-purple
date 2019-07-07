e0563bfc61010ddf7250fd1225e1a45135fd4b2e
aviau <alexandre@alexandreviau.net>
2019-07-07 18:42:37 -0400

    test_plugins: check the number of plugins

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -13,6 +13,7 @@ def test_plugins_get_protocols(core):
     protocols = plugins.get_protocols()
     assert isinstance(protocols, list)
     assert protocols
+    assert len(protocols) > 3
 
 
 def test_plugins_enabled():
@@ -33,6 +34,7 @@ def test_plugins_get_all(core):
     plugins = purple_plugins.get_plugins()
     assert isinstance(plugins, list)
     assert plugins
+    assert len(plugins) > 3
 
 
 def test_plugins_get_search_paths_no_core():
@@ -47,3 +49,4 @@ def test_plugins_get_search_paths(core):
     search_paths = purple_plugins.get_search_paths()
     assert isinstance(search_paths, list)
     assert search_paths
+    assert len(search_paths) == 1
