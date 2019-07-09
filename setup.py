0d924fde22382abbd4622957e3706380ad604d3d
aviau <alexandre@alexandreviau.net>
2019-07-09 02:07:54 -0400

    setup.py: remove unused import

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 from subprocess import Popen, PIPE
