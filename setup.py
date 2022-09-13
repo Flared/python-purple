d03bca8ff1553e83c1c3537d9592724bec445ef5
Mathieu Gascon-Lefebvre <mathieu.lefebvre@flare.systems>
2022-09-13 11:40:57 -0400

    Prepare 0.1.5

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ Python bindings for libpurple, a multi-protocol instant messaging library."
 
 setup(
     name="python-purple",
-    version="0.1.4",
+    version="0.1.5",
     author="Alexandre Viau",
     author_email="alexandre.viau@flare.systems",
     description="Python bindings for libpurple",
